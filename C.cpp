56b69af0463dab8a43272c8ef72fc7e26611de4a
Karvalian <obaid8197@gmail.com>
2021-05-15 10:01:10 -0700

    first commit

---
+++ C.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	// Amount of times the program has to run
+	int t;
+	cin >> t;
+	// end
+	while(t--){
+		// Two numbers to find the remainder of
+		int a, b;
+		cin >> a >> b;
+		// end
+		// Simple formula to get the remainder
+		cout << a % b << endl;
+	}
+	
+}
